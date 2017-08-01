718f6141fc025fbe29728d9f527e4dbbbf5ea4ef
rambo <chengwenjun@yunjingit.com>
2017-08-01 18:24:54 +0800

    init commit

---
+++ wsgiapp.py
@@ -0,0 +1,8 @@
+
+
+
+def app(environ, start_response):
+    status = '200 Ok'
+    response_headers = [('Content-Type', 'text/plain')]
+    start_response(status, response_headers)
+    return ['Hello world from a simple WSGI application!\n']
